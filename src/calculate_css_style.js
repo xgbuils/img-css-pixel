e889e7b3d02792f0cf4a19440b7470fec96b84e3
Xavier Garcia Buils <xgbuils@gmail.com>
2018-01-21 23:18:58 +0100

    extract css color calculation to a new function

--- src/calculate_css_style.js
+++ src/calculate_css_style.js
@@ -1,22 +1,20 @@
 var reduce = require('./iterator_reduce')
 var GridIterator = require('./grid_iterator')
+var arrayToCssColor = require('./array_to_css_color')
 
 function calculateCssStyle(buffer, options) {
     var gridIterator = new GridIterator(options)
     var size = options.size
+    var calculateCellAverageColor = options.calculateCellAverageColor
     var boxShadow = reduce(gridIterator, function(acc, position) {
-        var averageColor = options.calculateCellAverageColor(buffer, {
+        var cssAverageColor = arrayToCssColor(calculateCellAverageColor(buffer, {
             position: position,
             size: options.size,
             grid: options.grid
-        })
-            .map(function(color) {
-                return ('00' + color.toString(16)).substr(-2, 2)
-            })
-            .join('')
+        }))
         var xCoord = size * position.x
         var yCoord = size * (position.y + 1)
-        acc.push(xCoord + 'px ' + yCoord + 'px 0 #' + averageColor)
+        acc.push(xCoord + 'px ' + yCoord + 'px 0 ' + cssAverageColor)
         return acc
     }, []).join(',')
     return 'box-shadow: ' + boxShadow + ';width:' + size + 'px;height:' + size + 'px'
--- test/index.js
+++ test/index.js
@@ -5,12 +5,14 @@ var gridIterator = require('./grid_iterator')
 var cellIterator = require('./cell_iterator')
 var calculateCellAverageColor = require('./calculate_cell_average_color')
 var calculateCssBoxShadow = require('./calculate_css_style')
+var arrayToCssColor = require('./array_to_css_color')
 
 var testSuites = [
     gridIterator,
     cellIterator,
     calculateCellAverageColor,
-    calculateCssBoxShadow
+    calculateCssBoxShadow,
+    arrayToCssColor
 ]
 
 testSuites.forEach(function(fn) {
